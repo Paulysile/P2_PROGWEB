5c91679936675f8aac9537f31ddb62bfef5d80e5
Alexisveraguas <al.veraguas@duocuc.cl>
2021-06-01 18:43:39 -0400

    Archivo seccion-perro
    Se actualizan las funciones para pagina de perros y el js relacionado.
    Angelo me lo envía, porque presenta problemas de conexión.

--- js/seccion-perros.js
+++ js/seccion-perros.js
@@ -1,15 +1,66 @@
+new Vue({
+    el: '#app',
+    vuetify: new Vuetify(),
+    data: {
+        country_flag_url: "",
+        images: [],
+        breeds: [],
+        selected_breed: {},
+        current_image: {}
+    },
+    created() {
+        this.getBreeds();
+    },
+    watch: {
+
+        selected_breed: function () {
+            console.log(this.selected_breed)
+
+            let country_code = this.selected_breed.country_code.toLowerCase();
+
+            this.country_flag_url = `https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.2.1/flags/1x1/${country_code}.svg`;
+            this.getImages();
+        }
+    },
+    methods: {
+
+        async getBreeds() {
+            try {
+                axios.defaults.headers.common['x-api-key'] = "DEMO-API-KEY" // Replace this with your API Key, as it's set to defaults it will be used from now onwards
+
+                let response = await axios.get('https://api.thedogapi.com/v1/breeds/')
+                this.breeds = response.data;
+                console.log("-- (" + this.breeds.length + ") Breeds from TheDogAPI.com")
+
+                // pick one to display initially
+                this.selected_breed = this.breeds[10]
+            } catch (err) {
+                console.log(err)
+            }
+        },
+        async getImages() {
+            try {
+
+                let query_params = {
+                    breed_ids: this.selected_breed.id,
+                    limit: 8
+                }
+
+                let response = await axios.get('https://api.thedogapi.com/v1/images/search', { params: query_params })
+
+                this.pagination_count = response.headers['pagination-count'];
+                this.images = response.data
+                this.current_image = this.images[0]
 
 
-$(document).ready(function() {
 
-    var nroTotalImagenes = 10;
-    var nroColumnasPorFila = 5;
-    var selectorTablaHTML = "#lista-perros"
-    var url = "https://api.thedogapi.com/v1/images/search?limit="+nroTotalImagenes
+                console.log("-- (" + this.images.length + ") Images from TheDogAPI.com")
+                console.log(this.pagination_count, 'images available for this query.')
 
-    generar_galeria_imagenes(selectorTablaHTML, nroTotalImagenes, nroColumnasPorFila, url,"Cambiar Imagen");
-});
+            } catch (err) {
+                console.log(err)
+            }
+        }
 
-function getBtnActionURL() {
-    return "https://api.thedogapi.com/v1/images/search?limit=1";
-}
+    }
+})
